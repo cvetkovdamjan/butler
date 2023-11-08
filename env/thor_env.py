19c0da6b2311fc94dd20e62f021d65d88a54956e
Damjan Cvetkov <cvetkovdamjan@gmail.com>
2023-11-08 21:29:12 +0100

    started implementing training script

---
+++ env/thor_env.py
@@ -0,0 +1,5 @@
+import cv2
+import copy
+import gen.constants as constants
+import numpy as np
+from collections import Counter, OrderedDict
---
+++ gen/argument_trajectory.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import json
+import glob
+import os
+import constants
+import cv2
+import shutil
+import numpy as np
+import argparse
+import threading
+import time
+import copy
+import random
+from utils.video_util import VideoSaver
+from utils.py_util import walklevel
